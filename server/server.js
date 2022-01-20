bb3759577326a8f1b2ed9406992bed735bf72b2e
Rafael Carlos De Souza <rf2014souza@gmail.com>
2022-01-20 20:01:45 -0300

    Atualizando Server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import express from "express"
 const __dirname = path.resolve()
 const app = express();
 
-//app.use(express.static(path.join(__dirname,'build'))) //  usar de forma estatica toos os arquivos da pasta "dist";
+//   app.use(express.static(path.join(__dirname,'build'))) //  usar de forma estatica toos os arquivos da pasta "dist";
 app.set('port', process.env.PORT || 3002);
 
 app.get('/*', function (req, res) {
